9de17f816d46fcac990ee4e69ba915310b0a0fa8
Marcel Nitsch <mnitsch@ashley.localdomain>
2018-03-05 17:30:49 +0100

    Added includes to header

--- source/static_quark_potential.h
+++ source/static_quark_potential.h
@@ -1,7 +1,22 @@
 #ifndef STATIC_QUARK_POTENTIAL_H__
 #define STATIC_QUARK_POTENTIAL_H__
 
-#include <gsl/gsl_rng.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/* #include <unistd.h>
+#include <fcntl.h> */
+#include <time.h>
+#include <math.h>
+
+#include <gsl/gsl_rng.h>    /* random number generation */
+#include <gsl/gsl_complex.h>
+#include <gsl/gsl_complex_math.h>
+#include <gsl/gsl_matrix_complex_double.h>
+#include <gsl/gsl_vector_complex_double.h>
+#include <gsl/gsl_blas.h>   /* Basic Linear Algebra Subprograms */
+#include <gsl/gsl_linalg.h>     /* Only for debugging (determinants) */
 
 typedef struct PAR {
     int n_configs;
@@ -16,5 +31,8 @@ typedef struct PAR {
     int n_corr;
     double beta;
 } PAR;
+
+
+int init_su2(PAR *par, gsl_matrix_complex **su2);
 
 #endif
