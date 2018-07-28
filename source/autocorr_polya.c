2727568e4a7044dffbf023c681e071328aae1870
Spielix <paulg-b@web.de>
2018-07-28 16:41:38 +0200

    updated C analysis file

--- source/autocorr_polya.c
+++ source/autocorr_polya.c
@@ -12,21 +12,14 @@
 
 #include "static_quark_potential.h"
 
-double *auto_covariance(double **data, double tot_mean, int *n_dataset, int n_replicas) {
-    double *cov = NULL;
-    int n_tot = 0;
-    cov = malloc(n_dataset[0] * sizeof(double));
-    for (int replica = 0; replica < n_replicas; replica++) 
-        n_tot += n_dataset[replica];
-    for (int time = 0; time < n_dataset[0]; time++) {
-        cov[time] = 0.;
-        for (int replica = 0; replica < n_replicas; replica++) {
-            for (int i = 0; i < n_dataset[replica] - time; i++) {
-                cov[time] += (data[replica][i] - tot_mean) * (data[replica][i + time] - tot_mean);
-            }
+double auto_covariance(double **data, double tot_mean, int *n_dataset, int n_replicas, int time, int n_tot) {
+    double cov = 0.;
+    for (int replica = 0; replica < n_replicas; replica++) {
+        for (int i = 0; i < n_dataset[replica] - time; i++) {
+            cov += (data[replica][i] - tot_mean) * (data[replica][i + time] - tot_mean);
         }
-        cov[time] /= (double)(n_tot - n_replicas * time);
     }
+    cov /= (double)(n_tot - n_replicas * time);
     return cov;
 }
 
@@ -40,40 +33,72 @@ double gamma_error(
     double *t_corr_err
 ) {
 	double integ_corr_time = 0.5, corr_time, g = 1, err;
-	double *cov = auto_covariance(data, tot_mean, n_dataset, n_replicas);
     int window, n_tot = 0;
-    for (int replica = 0; replica < n_replicas; replica++) 
-        n_tot += n_dataset[replica];
-	
-    if (std_dev) 
-        *std_dev = sqrt(n_tot * cov[0] / (n_tot - 1));
+    for (int replica = 0; replica < n_replicas; replica++) n_tot += n_dataset[replica];
+	double cov_0 = auto_covariance(data, tot_mean, n_dataset, n_replicas, 0, n_tot);
+    if (std_dev) *std_dev = sqrt(n_tot * cov_0 / (n_tot - 1));
 
 	for(window = 1; (window < n_dataset[0]) && (g > 0); window++){
-		integ_corr_time += cov[window] / cov[0];
+        double cov_w = auto_covariance(data, tot_mean, n_dataset, n_replicas, window, n_tot);
+		integ_corr_time += cov_w / cov_0;
 		corr_time = 1 / log((2 * integ_corr_time + 1) / (2 * integ_corr_time - 1));
 		g = exp(-((double)window / corr_time)) - corr_time / sqrt((double)(window * n_tot));
 	}
     integ_corr_time *= 1. + (2. * (double)window + 1.) / (double)n_tot;
-	if (t_corr) 
-        *t_corr = integ_corr_time;
-    if (t_corr_err) 
-        *t_corr_err = 4. * ((double)window + 0.5 - integ_corr_time) * integ_corr_time * integ_corr_time / (double)n_tot;
+	if (t_corr) *t_corr = integ_corr_time;
+    if (t_corr_err) *t_corr_err = 4. * ((double)window + 0.5 - integ_corr_time) * integ_corr_time * integ_corr_time / (double)n_tot;
 
 
-	err = sqrt(2. * integ_corr_time * cov[0] / (double)n_tot);
-	free(cov);
+	err = sqrt(2. * integ_corr_time * cov_0 / (double)n_tot);
     return err;
 }
 
 int analyze_datasets(int arg_offset, char **argv, int n_replicas) {
     char name[1000], s_temp[1000];
-    double tot_mean, std_dev, t_corr, err, t_corr_err, beta;
-    double **data = NULL, *means = NULL, *n_dataset_fl = NULL;
-    int *n_dataset = NULL, L_t;
+    double tot_mean, 
+           tot_mean_abs, 
+           tot_mean2, 
+           tot_mean4, 
+           std_dev, 
+           std_dev_abs, 
+           std_dev2, 
+           std_dev4, 
+           t_corr, 
+           t_corr_abs, 
+           t_corr2, 
+           t_corr4, 
+           err, 
+           err_abs, 
+           err2, 
+           err4, 
+           t_corr_err, 
+           t_corr_err_abs, 
+           t_corr_err2, 
+           t_corr_err4, 
+           beta, 
+           cov_2_4_tot;
+    double **data = NULL, 
+           **data_abs = NULL, 
+           **data2 = NULL, 
+           **data4 = NULL, 
+           *means = NULL, 
+           *means_abs = NULL, 
+           *means2 = NULL, 
+           *means4 = NULL, 
+           *n_dataset_fl = NULL, 
+           *cov_2_4 = NULL;
+    int *n_dataset = NULL, L_t, L;
     data = malloc(n_replicas * sizeof(double *));
+    data_abs = malloc(n_replicas * sizeof(double *));
+    data2 = malloc(n_replicas * sizeof(double *));
+    data4 = malloc(n_replicas * sizeof(double *));
     means = malloc(n_replicas * sizeof(double));
+    means_abs = malloc(n_replicas * sizeof(double));
+    means2 = malloc(n_replicas * sizeof(double));
+    means4 = malloc(n_replicas * sizeof(double));
     n_dataset = malloc(n_replicas * sizeof(int));
     n_dataset_fl = malloc(n_replicas * sizeof(double));
+    cov_2_4 = malloc(n_replicas * sizeof(double));
     if ((data == NULL) || (means == NULL) || (n_dataset == NULL) || (n_dataset_fl == NULL)) {
         printf("Error: Failed allocating memory for data! Exiting...\n");
         return 1;
@@ -94,8 +119,9 @@ int analyze_datasets(int arg_offset, char **argv, int n_replicas) {
         if (replica == 0) {
             beta = par.beta;
             L_t = par.L_t;
+            L = par.L;
         } else {
-            if ((par.beta != beta) || (par.L_t != L_t)) {
+            if ((par.beta != beta) || (par.L_t != L_t) || (par.L != L)) {
                 printf("Error: simulation Parameters aren't the same! Exiting...");
                 return 1;
             }
@@ -112,22 +138,79 @@ int analyze_datasets(int arg_offset, char **argv, int n_replicas) {
         }
 
         data[replica] = malloc(n_dataset[replica] * sizeof(double));
+        data_abs[replica] = malloc(n_dataset[replica] * sizeof(double));
+        data2[replica] = malloc(n_dataset[replica] * sizeof(double));
+        data4[replica] = malloc(n_dataset[replica] * sizeof(double));
 
         for(int i = 0; i < par.n_configs; i++){
             fread(data[replica] + i, sizeof(double), 1, input);
         }
+        for (int i = 0; i < par.n_configs; i++) {
+            data_abs[replica][i] = fabs(data[replica][i]);
+            data2[replica][i] = data[replica][i] * data[replica][i];
+            data4[replica][i] = data2[replica][i] * data2[replica][i];
+        }
         fclose(input);
 
         means[replica] = gsl_stats_mean(data[replica], 1, par.n_configs);
+        means_abs[replica] = gsl_stats_mean(data_abs[replica], 1, n_dataset[replica]);
+        means2[replica] = gsl_stats_mean(data2[replica], 1, n_dataset[replica]);
+        means4[replica] = gsl_stats_mean(data4[replica], 1, n_dataset[replica]);
+        cov_2_4[replica] = gsl_stats_covariance_m(data2[replica], 1, data4[replica], 1, n_dataset[replica], means2[replica], means4[replica]);
     }
     
     tot_mean = gsl_stats_wmean(n_dataset_fl, 1, means, 1, n_replicas);
+    tot_mean_abs = gsl_stats_wmean(n_dataset_fl, 1, means_abs, 1, n_replicas);
+    tot_mean2 = gsl_stats_wmean(n_dataset_fl, 1, means2, 1, n_replicas);
+    tot_mean4 = gsl_stats_wmean(n_dataset_fl, 1, means4, 1, n_replicas);
+    cov_2_4_tot = gsl_stats_wmean(n_dataset_fl, 1, cov_2_4, 1, n_replicas);
 
-    for (int replica = 0; replica < n_replicas; replica++) 
+    for (int replica = 0; replica < n_replicas; replica++) {
         err = gamma_error(data, tot_mean, n_dataset, n_replicas, &std_dev, &t_corr, &t_corr_err);
+        err_abs = gamma_error(data_abs, tot_mean_abs, n_dataset, n_replicas, &std_dev_abs, &t_corr_abs, &t_corr_err_abs);
+        err2 = gamma_error(data2, tot_mean2, n_dataset, n_replicas, &std_dev2, &t_corr2, &t_corr_err2);
+        err4 = gamma_error(data4, tot_mean4, n_dataset, n_replicas, &std_dev4, &t_corr4, &t_corr_err4);
 //        err = error_auto_weight_simple(data[0], tot_mean, n_dataset[0], &std_dev, &t_corr);
-    
-    printf("%f\t%d\t%e\t%e\t%e\t%e\n", beta, L_t, tot_mean, err, t_corr, t_corr_err);
+    }
+    free(n_dataset);
+    free(n_dataset_fl);
+    free(cov_2_4);
+    for (int replica = 0; replica < n_replicas; replica++) { 
+        free(data[replica]);
+        free(data_abs[replica]);
+        free(data2[replica]);
+        free(data4[replica]);
+    }
+    free(data);
+    free(data_abs);
+    free(data2);
+    free(data4);
+    free(means);
+    free(means_abs);
+    free(means2);
+    free(means4);
+    printf("%g %d %d %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g\n", 
+            beta, 
+            L_t, 
+            L, 
+            tot_mean, 
+            err, 
+            t_corr, 
+            std_dev,
+            tot_mean_abs, 
+            err_abs, 
+            t_corr_abs, 
+            std_dev_abs, 
+            tot_mean2, 
+            err2, 
+            t_corr2, 
+            std_dev2, 
+            tot_mean4, 
+            err4, 
+            t_corr4, 
+            std_dev4, 
+            cov_2_4_tot
+    );
     return 0;
 }
 
