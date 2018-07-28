7902a43da76c9a1cc2815241a7ffdaf2d5748ea5
Spielix <paulg-b@web.de>
2018-07-28 16:39:44 +0200

    got data, data analysis python code

---
+++ finite_size_scaling_data/analysis.py
@@ -0,0 +1,32 @@
+import os 
+
+os.system("ls L12*a.bin > dummy.txt") 
+fin = open("dummy.txt")
+
+inlist = fin.readlines()
+
+fin.close()
+#inlist = os.listdir(./L*)
+
+#print(inlist)
+
+for i in range(len(inlist)):
+    inlist[i] = (inlist[i])[:-1]
+
+#print(inlist)
+
+outstr = "./autocorr_polya"
+
+for j in range(len(inlist)):
+    outstr += " 1 "
+#    print(j)
+#    outstr += str(11 + j)
+#    outstr += ".bin "
+    outstr += inlist[j]
+#    outstr += " " 
+#    outstr += inlist[2*j+1]
+#    print(outstr)
+    
+os.system(outstr)
+os.remove("dummy.txt")
+
